2e3e54d267097292a91130b3692f1a0431664806
tbecker <tylerjohnbecker@gmail.com>
2023-11-14 19:02:51 -0800

    just a quick change to make it so that the robot mover etc. work by taking a robot_prefix as input so that they can be used for multiple different robots

--- week4/scripts/robot_mover.py
+++ week4/scripts/robot_mover.py
@@ -8,75 +8,7 @@ from geometry_msgs.msg import Twist, Pose
 from tf.transformations import euler_from_quaternion
 from nav_msgs.msg import Odometry
 from sensor_msgs.msg import LaserScan
-
-class Object:
-
-	def __init__(self, midpoint, gains, q_star):
-		self.midpoint 		= copy.deepcopy(midpoint)
-		self.gains 			= copy.deepcopy(gains)
-		self.q_star 		= copy.deepcopy(q_star)
-
-class ObjectIdentifier:
-
-	def __init__ (self, odom):
-		self.objects 		= []
-		self.gains 			= np.array([.10, .10, .10])
-		self.q_star			= 3.0
-		self.odometry		= odom
-
-		self.laser_sub		= rospy.Subscriber('/robot_0/base_scan', LaserScan, self.laserCallback)
-
-	def laserCallback(self, data):
-		
-		n_objects = []
-
-		# print('hello')
-
-		true_angle_min = self.odometry.orientation[2] + data.angle_min
-
-		for index, dist in enumerate(data.ranges):
-
-			if dist <= self.q_star:
-
-				dist_x = dist * math.cos( true_angle_min + data.angle_increment * index )
-				dist_y = dist * math.sin( true_angle_min + data.angle_increment * index )
-
-				midpoint = np.array([self.odometry.position[0] + dist_x, self.odometry.position[1] + dist_y, self.odometry.position[2]])
-
-				# print(self.odometry.position, dist, (true_angle_min + data.angle_increment * index), dist_x, dist_y, midpoint)
-
-				n_objects.append( Object(midpoint, self.gains, self.q_star) )
-
-		self.objects = n_objects
-
-
-class GroupIdentifier:
-
-	def __init__(self):
-		self.objects 		= []
-
-	def groupCallback(self, data):
-		pass
-
-class OdomListener:
-
-	def __init__(self):
-		self.position 		= np.array([0, 0, 0])
-		self.orientation	= np.array([0, 0, 0])
-		self.lin_velocity	= np.array([0 ,0, 0])
-		self.ang_velocity	= np.array([0 ,0, 0])
-
-		self.odom_sub 		= rospy.Subscriber('/robot_0/base_pose_ground_truth', Odometry, self.odomCallback)
-
-	def odomCallback(self, data):
-		self.position 		= np.array([data.pose.pose.position.x, data.pose.pose.position.y, data.pose.pose.position.z])
-
-		( euler_x, euler_y, euler_z ) = euler_from_quaternion([ data.pose.pose.orientation.x, data.pose.pose.orientation.y, data.pose.pose.orientation.z, data.pose.pose.orientation.w ])
-
-		self.orientation 	= np.array([euler_x, euler_y, euler_z])
-
-		self.lin_velocity	= np.array([data.twist.twist.linear.x, data.twist.twist.linear.y, data.twist.twist.linear.z])
-		self.ang_velocity	= np.array([data.twist.twist.angular.x, data.twist.twist.angular.y, data.twist.twist.angular.z])
+from week4.msg import Group
 
 def get_smallest_dist_and_direction(ang1, ang2):
 
@@ -103,13 +35,95 @@ def get_smallest_dist_and_direction(ang1, ang2):
     else:
         return distance1, 'right'
 
+class Object:
+
+	def __init__(self, midpoint, gains, q_star):
+		self.midpoint 		= copy.deepcopy(midpoint)
+		self.gains 			= copy.deepcopy(gains)
+		self.q_star 		= copy.deepcopy(q_star)
+
+class ObjectIdentifier:
+
+	def __init__ (self, robot_prefix, odom):
+		self.objects 		= []
+		self.gains 			= np.array([.10, .10, .10])
+		self.q_star			= 3.0
+		self.odometry		= odom
+
+		self.laser_sub		= rospy.Subscriber(robot_prefix + '/base_scan', LaserScan, self.laserCallback)
+
+	def laserCallback(self, data):
+		
+		n_objects = []
+
+		true_angle_min = self.odometry.orientation[2] + data.angle_min
+
+		for index, dist in enumerate(data.ranges):
+
+			if dist <= self.q_star:
+
+				dist_x = dist * math.cos( true_angle_min + data.angle_increment * index )
+				dist_y = dist * math.sin( true_angle_min + data.angle_increment * index )
+
+				midpoint = np.array([self.odometry.position[0] + dist_x, self.odometry.position[1] + dist_y, self.odometry.position[2]])
+
+				n_objects.append( Object(midpoint, self.gains, self.q_star) )
+
+		self.objects = n_objects
+
+
+class GroupIdentifier:
+
+	def __init__(self):
+		self.objects 		= []
+		self.attr_gains		= np.array([.1, .1, .1])
+		self.buffer 		= 2.0 
+
+		self.group_sub		= rospy.Subscriber(robot_prefix + '/detected_groups', Group, self.groupCallback)
+
+	def groupCallback(self, data):
+		
+		self.objects = []
+
+		midpoint = np.array([data.midpoint.position.x, data.midpoint.position.y, data.midpoint.position.z])
+
+		if data.type == 'circle':
+
+			n_obj = Object(midpoint, self.gains, data.size + self.buffer)
+
+			self.objects.append(n_obj)
+
+		elif data.type == 'line':
+
+			future_point			
+
+class OdomListener:
+
+	def __init__(self, robot_prefix):
+		self.position 		= np.array([0, 0, 0])
+		self.orientation	= np.array([0, 0, 0])
+		self.lin_velocity	= np.array([0 ,0, 0])
+		self.ang_velocity	= np.array([0 ,0, 0])
+
+		self.odom_sub 		= rospy.Subscriber(robot_prefix + '/base_pose_ground_truth', Odometry, self.odomCallback)
+
+	def odomCallback(self, data):
+		self.position 		= np.array([data.pose.pose.position.x, data.pose.pose.position.y, data.pose.pose.position.z])
+
+		( euler_x, euler_y, euler_z ) = euler_from_quaternion([ data.pose.pose.orientation.x, data.pose.pose.orientation.y, data.pose.pose.orientation.z, data.pose.pose.orientation.w ])
+
+		self.orientation 	= np.array([euler_x, euler_y, euler_z])
+
+		self.lin_velocity	= np.array([data.twist.twist.linear.x, data.twist.twist.linear.y, data.twist.twist.linear.z])
+		self.ang_velocity	= np.array([data.twist.twist.angular.x, data.twist.twist.angular.y, data.twist.twist.angular.z])
+
 class RobotMover:
 
-	def __init__(self, odom):
+	def __init__(self, robot_prefix, odom):
 		self.twist 			= Twist()
 		self.odometry 		= odom
 
-		self.twist_pub 		= rospy.Publisher('/robot_0/cmd_vel', Twist, queue_size=1)
+		self.twist_pub 		= rospy.Publisher(robot_prefix + '/cmd_vel', Twist, queue_size=1)
 
 	def publishTwist(self):
 		try:
@@ -136,11 +150,11 @@ class PotentialFieldController:
 
 	tolerance = .5
 
-	def __init__ (self, tol):
+	def __init__ (self, robot_prefix, tol):
 		self.groups 		= GroupIdentifier()
-		self.odom			= OdomListener()
-		self.objects 		= ObjectIdentifier(self.odom)
-		self.mover 			= RobotMover(self.odom)
+		self.odom			= OdomListener(robot_prefix)
+		self.objects 		= ObjectIdentifier(robot_prefix, self.odom)
+		self.mover 			= RobotMover(robot_prefix, self.odom)
 
 		self.goal 			= np.array([0, 0, 0])
 		self.obstacles  	= []
@@ -150,8 +164,12 @@ class PotentialFieldController:
 		self.obstacles = []
 
 		objects = copy.deepcopy(self.objects.objects)
+		groups = copy.deepcopy(self.groups.objects)
 
 		for i in objects:
+			self.obstacles.append(i)
+
+		for i in groups:
 			self.obstacles.append(i)
 
 	def calculateForce(self):
@@ -194,12 +212,13 @@ class PotentialFieldController:
 			self.mover.publishTwist()
 
 def main():
-	pass
+
+	pfc = PotentialFieldController(robot_prefix='/robot_0', tol=.5)
+
+	pfc.navigate(np.array([-6, 6, 0]))
 
 if __name__ == '__main__':
 
 	rospy.init_node('intelligent_mover', anonymous=True)
 
-	pfc = PotentialFieldController(tol=.5)
-
-	pfc.navigate(np.array([-6, 6, 0]))
+	main()
